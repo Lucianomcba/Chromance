419e790f5a266053954906cf5787e9922d61cc38
Justin Schuhmann <jmschu02@gmail.com>
2023-02-15 22:17:50 -0500

    updated with webui

--- chromance-firmware/src/main.cpp
+++ chromance-firmware/src/main.cpp
@@ -1,13 +1,6 @@
-/*
-   Chromance wall hexagon source (emotion controlled w/ EmotiBit)
-   Partially cribbed from the DotStar example
-   I smooshed in the ESP32 BasicOTA sketch, too
-
-   (C) Voidstar Lab 2021
-*/
-
-#include <Arduino.h>
-
+#include <ESP8266React.h>
+#include <LightMqttSettingsService.h>
+#include <LightStateService.h>
 #include "variables.h"
 
 #ifdef USING_DOTSTAR
@@ -17,30 +10,25 @@
 #include <Adafruit_NeoPixel.h>
 #endif
 
-#include <SPI.h>
-#include <WiFi.h>
 #include "time.h"
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 #include "mapping.h"
 #include "ripple.h"
 
-#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+#define SERIAL_BAUD_RATE 115200
 
 // Function declarations
-void connectToWiFi(const char *ssid, const char *pwd);
+void connectToWiFi(const char* ssid, const char* pwd);
 void setupOTA(void);
 void continueAnimation(void);
 void fade(void);
 void advanceRipple(void);
-void Task1code(void *pvParameters);
+void Task1code(void* pvParameters);
 void getNextAnimation(void);
 void loop(void);
 void setup(void);
 // End Function declarations
 
-const char *ntpServer = "pool.ntp.org";
+const char* ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = -14400;
 const int daylightOffset_sec = 3600;
 // sleep 10 hours
@@ -70,36 +58,10 @@ byte ledColors[NUMBER_OF_SEGMENTS][LEDS_PER_SEGMENTS][NUMBER_OF_STRIPS - 1];
 
 // These ripples are endlessly reused so we don't need to do any memory management
 Ripple ripples[NUMBER_OF_RIPPLES] = {
-    Ripple(0),
-    Ripple(1),
-    Ripple(2),
-    Ripple(3),
-    Ripple(4),
-    Ripple(5),
-    Ripple(6),
-    Ripple(7),
-    Ripple(8),
-    Ripple(9),
-    Ripple(10),
-    Ripple(11),
-    Ripple(12),
-    Ripple(13),
-    Ripple(14),
-    Ripple(15),
-    Ripple(16),
-    Ripple(17),
-    Ripple(18),
-    Ripple(19),
-    Ripple(20),
-    Ripple(21),
-    Ripple(22),
-    Ripple(23),
-    Ripple(24),
-    Ripple(25),
-    Ripple(26),
-    Ripple(27),
-    Ripple(28),
-    Ripple(29),
+    Ripple(0),  Ripple(1),  Ripple(2),  Ripple(3),  Ripple(4),  Ripple(5),  Ripple(6),  Ripple(7),
+    Ripple(8),  Ripple(9),  Ripple(10), Ripple(11), Ripple(12), Ripple(13), Ripple(14), Ripple(15),
+    Ripple(16), Ripple(17), Ripple(18), Ripple(19), Ripple(20), Ripple(21), Ripple(22), Ripple(23),
+    Ripple(24), Ripple(25), Ripple(26), Ripple(27), Ripple(28), Ripple(29),
 };
 
 unsigned int baseColor = random(0xFFFF);
@@ -110,132 +72,45 @@ byte numberOfAutoPulseTypes = randomPulsesEnabled + cubePulsesEnabled + starburs
 
 unsigned long nextSimulatedHeartbeat;
 unsigned long nextSimulatedEda;
+bool continueAnimationBool = true;
 
-// Task for running on Core 0
-TaskHandle_t HandleArduinoOTA_Task;
+AsyncWebServer server(80);
+ESP8266React esp8266React(&server);
+LightMqttSettingsService lightMqttSettingsService =
+    LightMqttSettingsService(&server, esp8266React.getFS(), esp8266React.getSecurityManager());
+LightStateService lightStateService = LightStateService(&server,
+                                                        esp8266React.getSecurityManager(),
+                                                        esp8266React.getMqttClient(),
+                                                        &lightMqttSettingsService);
 
-// Thread for running on opposite thread as loop
-void HandleArduinoOTA(void *pvParameters)
-{
-  Serial.print("Task1 running on core ");
-  Serial.println(xPortGetCoreID());
-
-  for (;;)
-  {
-    ArduinoOTA.handle();
-
-    struct tm timeinfo;
-    if (!getLocalTime(&timeinfo))
-    {
-      Serial.println("Failed to obtain time");
-      return;
-    }
-
-    if (timeinfo.tm_hour >= 22 || timeinfo.tm_hour <= 1)
-    {
-      esp_deep_sleep_start();
-    }
-
-    // only check for OTA update every 1/2 second
-    delay(500);
-  }
-}
-
-void setupOTA()
-{
-  // Start OTA server.
-  ArduinoOTA.setHostname(HOSTNAME);
-  ArduinoOTA.setPassword("esp32password");
-  // Wireless OTA updating? On an ARDUINO?! It's more likely than you think!
-  ArduinoOTA
-      .onStart([]()
-               {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-    
-    activeOTAUpdate= true;
-    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type); });
-  ArduinoOTA.onEnd([]()
-                   { Serial.println("\nEnd"); });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-                        { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); });
-  ArduinoOTA.onError([](ota_error_t error)
-                     {
-                      activeOTAUpdate= false;
-                       Serial.printf("Error[%u]: ", error);
-                       if (error == OTA_AUTH_ERROR)
-                         Serial.println("Auth Failed");
-                       else if (error == OTA_BEGIN_ERROR)
-                         Serial.println("Begin Failed");
-                       else if (error == OTA_CONNECT_ERROR)
-                         Serial.println("Connect Failed");
-                       else if (error == OTA_RECEIVE_ERROR)
-                         Serial.println("Receive Failed");
-                       else if (error == OTA_END_ERROR)
-                         Serial.println("End Failed"); });
-
-  ArduinoOTA.begin();
-
-  // loop and setup are pinned to core 1
-  xTaskCreatePinnedToCore(
-      HandleArduinoOTA,        /* Task function. */
-      "HandleArduinoOTA_Task", /* name of task. */
-      10000,                   /* Stack size of task */
-      NULL,                    /* parameter of the task */
-      1,                       /* priority of the task */
-      &HandleArduinoOTA_Task,  /* Task handle to keep track of created task */
-      0);                      /* pin task to core 0 */
-}
-
-void connectToWiFi()
-{
-  if (connected == true)
-    return;
-
-  WiFiManager wifiManager;
-
-  wifiManager.setWiFiAutoReconnect(true);
-  wifiManager.autoConnect(HOSTNAME);
-  Serial.printf("WiFi connected! IP address: %s", WiFi.localIP());
-  connected = true;
-}
-
-void getNextAnimation()
-{
-  if (currentAutoPulseType == 255 || (numberOfAutoPulseTypes > 1 && millis() - lastAutoPulseChange >= RIPPES_TIMEOUT))
-  {
+void getNextAnimation() {
+  if (currentAutoPulseType == 255 || (numberOfAutoPulseTypes > 1 && millis() - lastAutoPulseChange >= RIPPES_TIMEOUT)) {
     byte possiblePulse = 255;
 
-    while (true)
-    {
+    while (true) {
       possiblePulse = random(NUMBER_OF_ANIMATIONS);
 
       if (possiblePulse == currentAutoPulseType)
         continue;
 
-      switch (possiblePulse)
-      {
-      case 0:
-        if (!randomPulsesEnabled)
-          continue;
-        break;
+      switch (possiblePulse) {
+        case 0:
+          if (!randomPulsesEnabled)
+            continue;
+          break;
 
-      case 1:
-        if (!cubePulsesEnabled)
-          continue;
-        break;
+        case 1:
+          if (!cubePulsesEnabled)
+            continue;
+          break;
 
-      case 2:
-        if (!starburstPulsesEnabled)
-          continue;
-        break;
+        case 2:
+          if (!starburstPulsesEnabled)
+            continue;
+          break;
 
-      default:
-        continue;
+        default:
+          continue;
       }
       currentAutoPulseType = possiblePulse;
       lastAutoPulseChange = millis();
@@ -246,15 +121,11 @@ void getNextAnimation()
 }
 
 /// Decays the color on each led
-void fade()
-{
+void fade() {
   // Fade all dots to create trails
-  for (int strip = 0; strip < NUMBER_OF_SEGMENTS; strip++)
-  {
-    for (int led = 0; led < LEDS_PER_SEGMENTS; led++)
-    {
-      for (int i = 0; i < (NUMBER_OF_STRIPS - 1); i++)
-      {
+  for (int strip = 0; strip < NUMBER_OF_SEGMENTS; strip++) {
+    for (int led = 0; led < LEDS_PER_SEGMENTS; led++) {
+      for (int i = 0; i < (NUMBER_OF_STRIPS - 1); i++) {
         ledColors[strip][led][i] *= decay;
       }
     }
@@ -262,10 +133,8 @@ void fade()
 }
 
 // Advances the ripple forward one pixel
-void advanceRipple()
-{
-  for (int i = 0; i < NUMBER_OF_RIPPLES; i++)
-  {
+void advanceRipple() {
+  for (int i = 0; i < NUMBER_OF_RIPPLES; i++) {
     ripples[i].advance(ledColors);
   }
 }
@@ -273,25 +142,17 @@ void advanceRipple()
 // Fades the animation
 // Advances the ripple
 // Updates the pixel color
-void continueAnimation()
-{
+void continueAnimation() {
   fade();
   advanceRipple();
 
-  for (int segment = 0; segment < NUMBER_OF_SEGMENTS; segment++)
-  {
-    for (int fromBottom = 0; fromBottom < LEDS_PER_SEGMENTS; fromBottom++)
-    {
+  for (int segment = 0; segment < NUMBER_OF_SEGMENTS; segment++) {
+    for (int fromBottom = 0; fromBottom < LEDS_PER_SEGMENTS; fromBottom++) {
       int strip = ledAssignments[segment][0];
-      int led = round(fmap(
-          fromBottom,
-          0, (LEDS_PER_SEGMENTS - 1),
-          ledAssignments[segment][2], ledAssignments[segment][1]));
+      int led =
+          round(fmap(fromBottom, 0, (LEDS_PER_SEGMENTS - 1), ledAssignments[segment][2], ledAssignments[segment][1]));
       strips[strip].setPixelColor(
-          led,
-          ledColors[segment][fromBottom][0],
-          ledColors[segment][fromBottom][1],
-          ledColors[segment][fromBottom][2]);
+          led, ledColors[segment][fromBottom][0], ledColors[segment][fromBottom][1], ledColors[segment][fromBottom][2]);
     }
   }
 
@@ -299,34 +160,23 @@ void continueAnimation()
     strips[i].show();
 }
 
-u_int32_t getRandomColor()
-{
+u_int32_t getRandomColor() {
   return strip0.ColorHSV(baseColor, 255, 255);
 }
 
-void randomPulse()
-{
+void randomPulse() {
   int node = funNodes[random(numberOfFunNodes)];
   while (node == lastAutoPulseNode)
     node = funNodes[random(numberOfFunNodes)];
 
   lastAutoPulseNode = node;
 
-  for (int i = 0; i < MAX_PATHS_PER_NODES; i++)
-  {
-    if (nodeConnections[node][i] >= 0)
-    {
-      for (int j = 0; j < NUMBER_OF_RIPPLES; j++)
-      {
-        if (ripples[j].state == STATE_DEAD)
-        {
+  for (int i = 0; i < MAX_PATHS_PER_NODES; i++) {
+    if (nodeConnections[node][i] >= 0) {
+      for (int j = 0; j < NUMBER_OF_RIPPLES; j++) {
+        if (ripples[j].state == STATE_DEAD) {
           ripples[j].start(
-              node,
-              i,
-              getRandomColor(),
-              float(random(100)) / 100.0 * .2 + .5,
-              ANIMATION_TIME,
-              BEHAVIOR_FEISTY);
+              node, i, getRandomColor(), float(random(100)) / 100.0 * .2 + .5, ANIMATION_TIME, BEHAVIOR_FEISTY);
 
           break;
         }
@@ -335,8 +185,7 @@ void randomPulse()
   }
 }
 
-void cubePulse()
-{
+void cubePulse() {
   int node = cubeNodes[random(numberOfCubeNodes)];
 
   while (node == lastAutoPulseNode)
@@ -346,21 +195,11 @@ void cubePulse()
 
   rippleBehavior behavior = random(2) ? BEHAVIOR_ALWAYS_LEFT : BEHAVIOR_ALWAYS_RIGHT;
 
-  for (int i = 0; i < MAX_PATHS_PER_NODES; i++)
-  {
-    if (nodeConnections[node][i] >= 0)
-    {
-      for (int j = 0; j < NUMBER_OF_RIPPLES; j++)
-      {
-        if (ripples[j].state == STATE_DEAD)
-        {
-          ripples[j].start(
-              node,
-              i,
-              getRandomColor(),
-              .8,
-              ANIMATION_TIME,
-              behavior);
+  for (int i = 0; i < MAX_PATHS_PER_NODES; i++) {
+    if (nodeConnections[node][i] >= 0) {
+      for (int j = 0; j < NUMBER_OF_RIPPLES; j++) {
+        if (ripples[j].state == STATE_DEAD) {
+          ripples[j].start(node, i, getRandomColor(), .8, ANIMATION_TIME, behavior);
 
           break;
         }
@@ -369,25 +208,16 @@ void cubePulse()
   }
 }
 
-void starburstPulse()
-{
+void starburstPulse() {
   rippleBehavior behavior = random(2) ? BEHAVIOR_ALWAYS_LEFT : BEHAVIOR_ALWAYS_RIGHT;
 
   lastAutoPulseNode = starburstNode;
 
-  for (int i = 0; i < MAX_PATHS_PER_NODES; i++)
-  {
-    for (int j = 0; j < NUMBER_OF_RIPPLES; j++)
-    {
-      if (ripples[j].state == STATE_DEAD)
-      {
+  for (int i = 0; i < MAX_PATHS_PER_NODES; i++) {
+    for (int j = 0; j < NUMBER_OF_RIPPLES; j++) {
+      if (ripples[j].state == STATE_DEAD) {
         ripples[j].start(
-            starburstNode,
-            i,
-            strip0.ColorHSV(baseColor + (0xFFFF / 6) * i, 255, 255),
-            .65,
-            2600,
-            behavior);
+            starburstNode, i, strip0.ColorHSV(baseColor + (0xFFFF / 6) * i, 255, 255), .65, 2600, behavior);
 
         break;
       }
@@ -395,80 +225,121 @@ void starburstPulse()
   }
 }
 
-void startAnimation(byte animation)
-{
-  switch (currentAutoPulseType)
-  {
-    // RANDOM PULSES
-  case 0:
-  {
-    randomPulse();
-    break;
-  }
+void startAnimation(byte animation) {
+  switch (currentAutoPulseType) {
+      // RANDOM PULSES
+    case 0: {
+      randomPulse();
+      break;
+    }
 
-  // cubePulsesEnabled
-  case 1:
-  {
-    cubePulse();
-    break;
-  }
+    // cubePulsesEnabled
+    case 1: {
+      cubePulse();
+      break;
+    }
 
-  // starburstPulsesEnabled
-  case 2:
-  {
-    starburstPulse();
-    break;
-  }
+    // starburstPulsesEnabled
+    case 2: {
+      starburstPulse();
+      break;
+    }
 
-  default:
-    break;
+    default:
+      break;
   }
 }
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("*** LET'S GOOOOO ***");
+// Task for running on Core 0
+TaskHandle_t HandleArduinoOTA_Task;
 
-  for (int i = 0; i < NUMBER_OF_STRIPS; i++)
-  {
+// Thread for running on opposite thread as loop
+void HandleArduinoOTA(void* pvParameters) {
+  Serial.print("Task1 running on core ");
+  Serial.println(xPortGetCoreID());
+
+  for (;;) {
+    // run the framework's loop function
+    esp8266React.loop();
+
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)) {
+      Serial.println("Failed to obtain time");
+      return;
+    }
+
+    if (timeinfo.tm_hour >= 22 || timeinfo.tm_hour <= 1) {
+      continueAnimationBool = false;
+      for (int i = 0; i < NUMBER_OF_STRIPS; i++) {
+        strips[i].clear();
+        strips[i].show();
+      }
+      esp_deep_sleep_start();
+    }
+  }
+}
+
+void setup() {
+  // start serial and filesystem
+  Serial.begin(SERIAL_BAUD_RATE);
+
+  // start the framework and demo project
+  esp8266React.begin();
+
+  // load the initial light settings
+  lightStateService.begin();
+
+  // start the light service
+  lightMqttSettingsService.begin();
+
+  // start the server
+  server.begin();
+  continueAnimationBool = true;
+
+  for (int i = 0; i < NUMBER_OF_STRIPS; i++) {
     strips[i].begin();
-    strips[i].setBrightness(255); // If your PSU sucks, use this to limit the current
+    strips[i].setBrightness(255);  // If your PSU sucks, use this to limit the current
     strips[i].show();
   }
 
-  connectToWiFi();
-  setupOTA();
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  // loop and setup are pinned to core 1
+  xTaskCreatePinnedToCore(HandleArduinoOTA,        /* Task function. */
+                          "HandleArduinoOTA_Task", /* name of task. */
+                          10000,                   /* Stack size of task */
+                          NULL,                    /* parameter of the task */
+                          1,                       /* priority of the task */
+                          &HandleArduinoOTA_Task,  /* Task handle to keep track of created task */
+                          0);                      /* pin task to core 0 */
 }
 
-void loop()
-{
+void loop() {
   // We are doing an OTA update, might as well just stop
-  if (activeOTAUpdate)
-  {
+  if (activeOTAUpdate) {
     return;
   }
 
-  continueAnimation();
+  if (continueAnimationBool) {
+    continueAnimation();
 
-  if (numberOfAutoPulseTypes && millis() - lastRandomPulse >= randomPulseTime)
-  {
-    // float lastColor = (float)baseColor / (float)0xFFFF * 360;
-    // while (true)
-    // {
-    baseColor = random(0xFFFF);
-    //   float currentColor = baseColor / 65535 * 360;
-    //   int hueDistance = min(abs(lastColor - currentColor), 360 - abs(lastColor - currentColor));
-    //   if (hueDistance > 20)
-    //     break;
-    // }
+    if (numberOfAutoPulseTypes && millis() - lastRandomPulse >= randomPulseTime) {
+      // float lastColor = (float)baseColor / (float)0xFFFF * 360;
+      // while (true)
+      // {
+      baseColor = random(0xFFFF);
+      //   float currentColor = baseColor / 65535 * 360;
+      //   int hueDistance = min(abs(lastColor - currentColor), 360 - abs(lastColor - currentColor));
+      //   if (hueDistance > 20)
+      //     break;
+      // }
 
-    // change animation every 30 seconds
-    getNextAnimation();
+      // change animation every 30 seconds
+      getNextAnimation();
 
-    startAnimation(currentAutoPulseType);
-    lastRandomPulse = millis();
+      startAnimation(currentAutoPulseType);
+      lastRandomPulse = millis();
+    }
   }
 }
